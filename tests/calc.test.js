dfcccf22954bf7a5df76de71254639a5a8ab7447
Thiagoavanco27 <146395678+Thiagoavanco27@users.noreply.github.com>
2023-12-22 20:57:47 +0000

    calc autualizado

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -1,6 +1,5 @@
-test("nome do test", () => {
-    console.log ("Essa funcção esta sendo chamada?");
-});
-test("testando", () => {
-    console.log("Testando")
-});
+const calculadora = require ("../models/calculadora.js");
+test("somar 2 + 2 deveria retornar 4", ()  =>{
+    const resultado = calculadora.somar(2,3);
+    expect(resultado).toBe(5);
+})
